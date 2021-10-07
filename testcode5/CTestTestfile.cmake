d9dd63c0ece85bcd857d2158a462d5d7c822e6a4
Nitrow <printz.a91@gmail.com>
2021-10-07 10:16:07 +0200

    cleaned up

--- testcode5/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/asger/arbejde/testcode5
-# Build directory: /home/asger/arbejde/testcode5
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
